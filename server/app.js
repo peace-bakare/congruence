e389cd8e9b9470584557f46c0a8ca9658b6e1013
iammadab <wisdom_ogwu@yahoo.com>
2019-06-10 00:30:50 +0100

    added connection to database

--- server/app.js
+++ server/app.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 8080;
 
 // app.use("/api/v1/projects", PROJECT)
 
+//Connect to mongodb
+mongoose.connect("mongodb://localhost/congruence", { useNewUrlParser: true })
+            .then(() => { console.log("Connected to db") })
+            .catch(err => { console.log("Error connecting to db", err) })
+
+
 app.listen(PORT, () => {
 	console.log("Application listening at port " + PORT);
 })
