55a6591d61eb86e2c453d423271d78b02b15e8ab
John Afolayan <jnafolayan@gmail.com>
2019-06-10 02:36:51 +0100

    feat: split controllers into functions and implementation

---
+++ server/resources/Project/projectFns.js
@@ -0,0 +1,13 @@
+const uuidv1 = require('uuid/v1');
+
+const Project = require('./projectModel');
+
+exports.createProject = function createProject(projectDTO) {
+  const project = new Project(projectDTO);
+  project.ref = uuidv1();
+  return project.save()
+};
+
+exports.getAllProjects = function getAllProjects() {
+  return Project.fetchAll()
+};
