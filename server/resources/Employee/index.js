afe6df9087710892d3361a14b275c1be6a3aabb5
iammadab <wisdom_ogwu@yahoo.com>
2019-06-10 04:35:52 +0100

    employee search based on craft

--- server/resources/Employee/index.js
+++ server/resources/Employee/index.js
@@ -3,8 +3,10 @@ const employeeRouter = express.Router()
 
 const registerEmployee = require("./employeeRegister")
 const loginEmployee = require("./employeeLogin")
+const employeeSearch = require("./employeeSearch")
 
 employeeRouter.post("/", registerEmployee.route)
 employeeRouter.post("/login", loginEmployee.route)
+employeeRouter.post("/search", employeeSearch.route)
 
 module.exports = employeeRouter
