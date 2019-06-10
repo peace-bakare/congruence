661bb12782affdf827574b16231c0dafb0b59fa9
iammadab <wisdom_ogwu@yahoo.com>
2019-06-10 03:30:15 +0100

    employee login: email exists validation

---
+++ server/resources/Employee/employeeLogin.js
@@ -0,0 +1,57 @@
+const Employee = require("./employeeModel")
+const { compare } = require("bcrypt")
+const { createValidator } = require("../../lib/validator")
+const { createError, sendSuccess } = require("../../lib/responseHandler")
+
+const loginEmployeeValidator = createValidator("email.string, password.string")
+
+function loginEmployeeFn({ email, password }){
+
+	return checkIfEmailExists()
+			.then(checkForCorrectPassword)
+
+	function checkIfEmailExists(){
+		return Employee.findOne({ email: email })
+				.then(handleExists)
+
+		function handleExists(employee){
+			if(!employee)
+				throw createError(403, "INVALID_USERNAME_PASSWORD")
+			return employee
+		}
+	}
+
+	function checkForCorrectPassword(employee){
+		return compare()
+	}
+
+} 
+
+function loginEmployeeRoute(req, res, next){
+	loginEmployeeValidator(req.body)
+		.catch(sendBadRequestError)
+		.then(() => loginEmployeeFn(req.body))
+		.then(createSuccessResponse)
+		.then(sendSuccessResponse)
+		.catch(next)
+
+
+	function sendBadRequestError(error){
+		throw createError(400, "BAD_REQUEST_BODY", error.errors)
+	}
+
+	function createSuccessResponse(){
+		return {
+			message: "Employee logged in successfully"
+		}
+	}
+
+	function sendSuccessResponse(response){
+		sendSuccess(res, 200, response)
+	}
+}
+
+module.exports = {
+	fn: loginEmployeeFn,
+	route: loginEmployeeRoute
+}
--- server/resources/Employee/index.js
+++ server/resources/Employee/index.js
@@ -2,7 +2,9 @@ const express = require("express")
 const employeeRouter = express.Router()
 
 const registerEmployee = require("./employeeRegister")
+const loginEmployee = require("./employeeLogin")
 
 employeeRouter.post("/", registerEmployee.route)
+employeeRouter.post("/login", loginEmployee.route)
 
 module.exports = employeeRouter
